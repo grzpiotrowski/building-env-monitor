580eb353aeec20c1d50aeee9d32fa5ad18d666a4
Grzegorz Piotrowski <geopiotrowski@gmail.com>
2022-12-03 20:38:32 +0000

    Pico reading temp and humidity continuously

--- README.md
+++ README.md
@@ -6,10 +6,16 @@
 
 
 # Resources
+
+## Raspberry Pi Pico
 * Getting started with Raspberry Pi Pico \
 https://projects.raspberrypi.org/en/projects/getting-started-with-the-pico/0 \
 
 * Raspberry Pi Pico Pinout \
 https://datasheets.raspberrypi.com/pico/Pico-R3-A4-Pinout.pdf \
+
+* MicroPython - DHT11
+https://docs.micropython.org/en/latest/index.html
+https://docs.micropython.org/en/latest/esp8266/tutorial/dht.html?highlight=dht11
 
 
--- temperature-humidity-unit/dht11TempHumidity.py
+++ temperature-humidity-unit/dht11TempHumidity.py
@@ -1,11 +1,15 @@
 import dht
 import machine
+from time import sleep
+
 dhtSensor = dht.DHT11(machine.Pin(22))
-
-dhtSensor.measure()
-
-temperature = dhtSensor.temperature()
-humidity = dhtSensor.humidity()
-
-print("temperature: " + str(temperature))
-print("humidity: " + str(humidity))
+i = 0
+while True:
+  dhtSensor.measure()
+  temperature = dhtSensor.temperature()
+  humidity = dhtSensor.humidity()
+  i += 1
+  print("Measurement: " + str(i))
+  print("temperature: " + str(temperature))
+  print("humidity: " + str(humidity))
+  sleep(5)
