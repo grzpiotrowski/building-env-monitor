024f46c2d0f2e379f6514e9ec304eeaf256c1173
Grzegorz Piotrowski <geopiotrowski@gmail.com>
2022-12-04 20:16:43 +0000

    Added Pico startup delay to allow DHT11 init, LED indicator

--- temperature-humidity-unit/dht11TempHumidity.py
+++ temperature-humidity-unit/dht11TempHumidity.py
@@ -6,6 +6,12 @@ from time import sleep
 # device id
 deviceId = "pico01"
 
+# Interval between measurements in seconds
+dataInterval = 5
+
+LED = Pin(25, Pin.OUT)
+ledBlinkTime = 1
+
 # setup the UART
 id = 0
 rx = Pin(1)
@@ -17,24 +23,25 @@ uart = UART(0, baudrate=baudrate,tx=tx, rx=rx)
 # create dht sensor instance
 dhtSensor = dht.DHT11(Pin(22))
 
+LED.value(1)
+sleep(5) # Sleep to let DHT11 initialise after powering up
+
 i = 0
 while True:
-    dhtSensor.measure()
-    temperature = dhtSensor.temperature()
-    humidity = dhtSensor.humidity()
-    i += 1
-    sensorMessage = '{ deviceid: "%s", temp: %.0f, humidity: %.0f }' % (
-        deviceId, temperature, humidity)
-    print(sensorMessage)
-    uart.write(sensorMessage)
-    sleep(5)
-
-
-
-
-
-
-
-
-
+    try:
+        dhtSensor.measure()
+        temperature = dhtSensor.temperature()
+        humidity = dhtSensor.humidity()
+        i += 1
+        sensorMessage = '{ deviceid: "%s", temp: %.0f, humidity: %.0f }' % (
+            deviceId, temperature, humidity)
+        #print(sensorMessage)
+        uart.write(sensorMessage)
+        LED.value(1)
+        sleep(ledBlinkTime)
+        LED.value(0)
+        sleep(dataInterval - ledBlinkTime)
+    except KeyboardInterrupt:
+        LED.value(0)
+        break
 
