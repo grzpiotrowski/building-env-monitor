80a2a93ff21a1b122f5d3eadca7df6b2b417eca1
Grzegorz Piotrowski <geopiotrowski@gmail.com>
2022-12-07 21:14:45 +0000

    Refactored mqttClient

--- raspberry-pi/mqttClient.py
+++ raspberry-pi/mqttClient.py
@@ -13,29 +13,41 @@ def on_connect(client, userdata, flags, rc):
 def on_publish(client, obj, mid):
     print("Message ID: " + str(mid))
 
-mqttc = mqtt.Client()
 
-# Assign event callbacks
-mqttc.on_connect = on_connect
-mqttc.on_publish = on_publish
+def createMqttClient():
+    mqttc = mqtt.Client()
+    # Assign event callbacks
+    mqttc.on_connect = on_connect
+    mqttc.on_publish = on_publish
 
-mqttBrokerURL = APIkeys.mqttBrokerURL
-mqttBrokerPort = APIkeys.mqttBrokerPort
-print(mqttBrokerURL)
+    mqttBrokerURL = APIkeys.mqttBrokerURL
+    mqttBrokerPort = APIkeys.mqttBrokerPort
+    print(mqttBrokerURL)
 
-# enable TLS for secure connection
-mqttc.tls_set(tls_version=mqtt.ssl.PROTOCOL_TLS)
+    # enable TLS for secure connection
+    mqttc.tls_set(tls_version=mqtt.ssl.PROTOCOL_TLS)
 
-# set username and password
-mqttc.username_pw_set(APIkeys.mqttUser, APIkeys.mqttPassword)
-# connect to MQTT Cloud on specified port
-mqttc.connect(mqttBrokerURL, mqttBrokerPort)
-mqttc.loop_start()
+    # set username and password
+    mqttc.username_pw_set(APIkeys.mqttUser, APIkeys.mqttPassword)
+    # connect to MQTT Cloud on specified port
+    mqttc.connect(mqttBrokerURL, mqttBrokerPort)
+    mqttc.loop_start()
 
-# Publish a message to temp every 15 seconds
-while True:
-    temp=round(random.uniform(15, 21), 1)
-    temp_json=json.dumps({'temperature':temp, 'timestamp':time.time()})
-    print(str(temp_json))
-    print(mqttc.publish('temperature', temp_json))
-    time.sleep(5)
+    return mqttc
+
+
+def publishMqtt(topic, payload):
+    print("Publishing: " + str(payload))
+    response = mqttc.publish(topic, payload)
+    print(response)
+
+
+if __name__ == "__main__":
+    mqttc = createMqttClient()
+
+    # Publish a message to temp every 5 seconds
+    while True:
+        temp=round(random.uniform(15, 21), 1)
+        temp_json=json.dumps({'temperature':temp, 'timestamp':time.time()})
+        publishMqtt('temperature', temp_json)
+        time.sleep(5)
