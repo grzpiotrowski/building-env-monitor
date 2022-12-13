88c94ee6dd3f37e5e0e996d22481a675c374ac69
Grzegorz Piotrowski <geopiotrowski@gmail.com>
2022-12-13 21:28:11 +0000

    Added unix timestamp to mqtt message

--- raspberry-pi/main.py
+++ raspberry-pi/main.py
@@ -1,9 +1,11 @@
 import mqttClient
 import rfcommClient
 import time
+import json
 
 
 if __name__ == "__main__":
+    topicName = "sensor-temp-humidity"
     bluetoothMAC = '98:D3:71:F6:5F:5D' # MAC address of the HC–05 transceiver
     port = 1
     clientSocket = rfcommClient.createSocket(bluetoothMAC, port)
@@ -12,8 +14,10 @@ if __name__ == "__main__":
 
     while True:
         try:
-            sensorData = rfcommClient.getData(clientSocket, '}')
-            mqttClient.publishMqtt(mqttc, 'temperature', sensorData)
+            sensorDataJson = rfcommClient.getData(clientSocket, '}')
+            sensorData = json.loads(sensorDataJson)
+            sensorData["timestamp"] = int(time.time())
+            mqttClient.publishMqtt(mqttc, topicName, json.dumps(sensorData))
             time.sleep(5)
         except KeyboardInterrupt:
             break
